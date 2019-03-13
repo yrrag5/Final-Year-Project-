724e80d9c6d75cd9a07e0e3a8aa8d657c0fd907b
yrrag5 <G00335806@gmit.ie>
2019-03-13 20:31:58 +0000

    Fixed problem with collectors

--- Spring/Intouch/src/main/java/com/restapp/Intouch/controller/ClubController.java
+++ Spring/Intouch/src/main/java/com/restapp/Intouch/controller/ClubController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Collection;
-import java.util.Collections;
+import java.util.stream.Collectors;
 
 public class ClubController {
     private ClubRepository repo;
@@ -18,7 +18,7 @@ public class ClubController {
     @CrossOrigin(origins = "http://localhost:4200")
     public Collection<Club> getClubs(){
         return repo.findAll().stream()
-                .collect(Collections.toList());
+                .collect(Collectors.toList());
 
     }
 }
