e55de69433fb0b6170c906a3e21b958bd1855760
yrrag5 <G00335806@gmit.ie>
2019-03-13 20:06:18 +0000

    Adding jpa dependency for interface. Created controller folder for api use with ionic

---
+++ Spring/Intouch/src/main/java/com/restapp/Intouch/controller/ClubController.java
@@ -0,0 +1,24 @@
+package com.restapp.Intouch.controller;
+
+import com.restapp.Intouch.models.Club;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class ClubController {
+    private ClubRepository repo;
+
+    public ClubController(ClubRepository repository){
+        this.repo = repo;
+    }
+
+    @GetMapping("/getClubs")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public Collection<Club> getClubs(){
+        return repo.findAll().stream()
+                .collect(Collections.toList());
+
+    }
+}
