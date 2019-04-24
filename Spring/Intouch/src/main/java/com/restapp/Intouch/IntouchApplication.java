180741eecb8ec352d138b0a216d7d258a521f2ef
yrrag5 <G00335806@gmit.ie>
2019-04-24 19:30:14 +0100

    CRUD operations all working

--- Spring/Intouch/src/main/java/com/restapp/Intouch/IntouchApplication.java
+++ Spring/Intouch/src/main/java/com/restapp/Intouch/IntouchApplication.java
@@ -17,39 +17,47 @@ public class IntouchApplication implements CommandLineRunner{
 
 
 	private static final Logger LOG = LoggerFactory.getLogger("IntouchApplication");
-
-	//private final ClubDal clubDAL;
-	private final UserDal userDAL;
-
-
-	/*@Autowired
-	public IntouchApplication(ClubDal clubDAL) {
-		this.clubDAL = clubDAL;
-	}*/
+	 private final ClubDal clubDAL;
+	 //private final UserDal userDAL;
+	 //public ClubDal clubDAL;
+	//public UserDal userDAL;
 
 
 	@Autowired
+	public IntouchApplication(ClubDal clubDAL) {
+		this.clubDAL = clubDAL;
+	}
+
+
+	/*@Autowired
 	public IntouchApplication(UserDal userDal) {
     	this.userDAL = userDal;
-	}
+	}*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntouchApplication.class, args);
 	}
 	@Override
 	public void run(String... args) {
-		//userDAL.saveUser(new User(  "Test", "Test", "Test", "Test", "Test"));
-       // userDAL.saveUser(new User(  "Test", "Test", "Test", "Test", "Test"));
+		/*userDAL.saveUser(new User(  "1", "Test", "Test", "Test", "Test"));
+        userDAL.saveUser(new User(  "2", "Test", "Test", "Test", "Test"));
+		userDAL.saveUser(new User(  "3", "Test", "Test", "Test", "Test"));
+		userDAL.saveUser(new User(  "4", "Test", "Test", "Test", "Test"));
+		userDAL.saveUser(new User(  "5", "Test", "Test", "Test", "Test"));*/
 
-        LOG.info("Getting all user data from MongoDB: \n{}",
-				userDAL.getAllUsers());
+        /*LOG.info("Getting all user data from MongoDB: \n{}",
+				userDAL.getAllUsers());*/
 
 
-		/*clubDAL.saveClub(new Club(  "gaa", "s21"));
-		clubDAL.saveClub(new Club("gfc", "t54"));
+
+
+		/*clubDAL.saveClub(new Club(  "123", "gaa", "s21", "1", "1"));
+		clubDAL.saveClub(new Club("456", "gfc", "t54", "1", "1"));
+		clubDAL.saveClub(new Club("789", "ssf", "t54", "1", "1"));
+		clubDAL.saveClub(new Club("234", "tty", "t54", "1", "1"));*/
 
 		LOG.info("Getting all data from MongoDB: \n{}",
-				clubDAL.getAllClubs());*?
+				clubDAL.getAllClubs());
 
 		/*LOG.info("Getting paginated data from MongoDB: \n{}",
 				personDAL.getAllPersonPaginated(0, 2));
---
+++ Spring/Intouch/src/main/java/com/restapp/Intouch/controllers/ClubController.java
@@ -0,0 +1,75 @@
+package com.restapp.Intouch.controllers;
+
+import javax.validation.Valid;
+
+import com.restapp.Intouch.models.Club;
+//import com.restapp.Intouch.models.User;
+import com.restapp.Intouch.repositories.ClubRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.List;
+
+@RestController
+//@RequestMapping("/api")
+//@CrossOrigin("*")
+public class ClubController {
+
+    @Autowired
+    ClubRepo clubRepo;
+
+    // Get users api
+    @GetMapping("/clubs")
+    @ResponseBody
+    @CrossOrigin(origins = "http://localhost:8200")
+    public Collection<Club> getAllClubs(){
+        return clubRepo.findAll();
+        //Sort sortByCreatedAtDesc = new Sort(Sort.Direction.DESC, "createdAt");
+        // return userRepo.findAll(sortByCreatedAtDesc);
+    }
+
+    // Create club
+    @PostMapping("/club")
+    public Club createClub(@Valid @RequestBody Club club) {
+        return clubRepo.save(club);
+    }
+
+   /* // Get club by id
+    @GetMapping(value="/user/{clubId}")
+    public ResponseEntity<User> getUserById(@PathVariable("clubId") String clubId) {
+        return clubRepo.findById(clubId)
+                .map(club -> ResponseEntity.ok().body(club))
+                .orElse(ResponseEntity.notFound().build());
+    }*/
+
+    // Update club
+    @PutMapping(value="/club/{clubId}")
+    public ResponseEntity<Club> updateClub(@PathVariable("clubId") String clubId,
+                                           @Valid @RequestBody Club club) {
+        return clubRepo.findById(clubId)
+                .map(clubData -> {
+
+                    clubData.setClubId(club.getClubId());
+                    clubData.setClubName(club.getClubName());
+                    clubData.setClubAddress(club.getClubAddress());
+                    clubData.setClubEmail(club.getClubEmail());
+                    clubData.setClubMobile(club.getClubMobile());
+
+                    Club updatedClub = clubRepo.save(clubData);
+                    return ResponseEntity.ok().body(updatedClub);
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
+    // Delete club
+    @DeleteMapping(value="/club/{clubId}")
+    public ResponseEntity<?> deleteClub(@PathVariable("clubId") String clubId) {
+        return clubRepo.findById(clubId)
+                .map(todo -> {
+                    clubRepo.deleteById(clubId);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
+    }
+}
