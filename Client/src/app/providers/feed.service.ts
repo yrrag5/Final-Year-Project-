f4ec5bce7fc18f363c4c9831a967298924fa7b5c
HUGHBRADY <G00338260@gmit.ie>
2019-04-25 15:22:45 +0100

    data finally being passed

--- Client/src/app/providers/feed.service.ts
+++ Client/src/app/providers/feed.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Response } from '@angular/http';
 import { map, filter } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
@@ -17,15 +16,17 @@ export class FeedService {
     return this.http.get(this.API + "/clubs")
       .pipe(map((clubs: any) => {
           console.log("Club table: ", clubs);
+          return clubs;
         })
       ); 
   }
 
   getUsers(): Observable<any> {
     return this.http.get(this.API + "/users")
-     .pipe(map((users: any) => {
-        console.log("User table: ", users);
-      })
-    );  
-  }
+        .pipe(map((users: any) => {
+            console.log("User table: ", users);
+            return users;
+        })
+     );  
+}
 }
